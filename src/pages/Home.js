04a7f19ec1c7506ca27d0011712523dccd700ebe
rusikstayhome <alenrus32@gmail.com>
2022-08-17 20:01:37 +0300

    added User info and chat

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { db, auth } from '../firebase';
+import { collection, query, where, onSnapshot, QuerySnapshot } from 'firebase/firestore';
+import User from "../components/User";
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const usersRef = collection(db, 'users');
+        const q = query(usersRef, where('uid', 'not-in', [auth.currentUser.uid]));
+        const unsub = onSnapshot(q, querySnapshot => {
+            let users = []
+            querySnapshot.forEach(doc => {
+                users.push(doc.data())
+            })
+            setUsers(users);
+        });
+        return () => unsub();
+    }, []);
+    console.log(users);
     return (
-        <div>
-            Homepage
+        <div className="home_container">
+            <div className="users_container">
+                {users.map(user => <User key={user.uid} user={user} />)}
+            </div>
         </div>
     )
 }
