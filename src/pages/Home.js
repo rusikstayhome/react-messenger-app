df7b608622e29f62c433222d478e4d916a854c73
rusikstayhome <alenrus32@gmail.com>
2022-08-18 13:36:34 +0300

    added a MessageForm

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { db, auth } from '../firebase';
-import { collection, query, where, onSnapshot, QuerySnapshot } from 'firebase/firestore';
+import { collection, query, where, onSnapshot, addDoc, Timestamp } from 'firebase/firestore';
 import User from "../components/User";
+import MessageForm from "../components/MessageForm";
 
 const Home = () => {
     const [users, setUsers] = useState([]);
+    const [chat, setChat] = useState('');
+    const [text, setText] = useState('');
 
+    const user1 = auth.currentUser.uid
     useEffect(() => {
         const usersRef = collection(db, 'users');
-        const q = query(usersRef, where('uid', 'not-in', [auth.currentUser.uid]));
+        const q = query(usersRef, where('uid', 'not-in', [user1]));
         const unsub = onSnapshot(q, querySnapshot => {
             let users = []
             querySnapshot.forEach(doc => {
@@ -18,11 +22,43 @@ const Home = () => {
         });
         return () => unsub();
     }, []);
-    console.log(users);
+
+    const selectUser = (user) => {
+        setChat(user);
+        console.log(user);
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const user2 = chat.uid
+
+        const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`
+
+        await addDoc(collection(db, 'messages', id, 'chat'), {
+            text,
+            from: user1,
+            to: user2,
+            createdAt: Timestamp.fromDate(new Date())
+        });
+        setText('');
+    }
     return (
         <div className="home_container">
             <div className="users_container">
-                {users.map(user => <User key={user.uid} user={user} />)}
+                {users.map(user => <User key={user.uid} user={user} selectUser={selectUser} />)}
+            </div>
+            <div className="messages_container">
+                {chat ? (
+                    <>
+                        <div className="messages_user">
+                            <h3>{chat.name}</h3>
+                        </div>
+                        <MessageForm handleSubmit={handleSubmit} text={text} setText={setText} />
+                    </>
+                ) : (
+                    <h3 className="no_conv">Select a user to start conversation </h3>
+                )}
             </div>
         </div>
     )
