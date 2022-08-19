251eae780be1397628f173c8271775bbdaeadc12
rusikstayhome <alenrus32@gmail.com>
2022-08-19 17:46:11 +0300

    added a user info in Profile

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -16,6 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app)
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { auth, db };
+export { auth, db, storage };
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,32 +1,77 @@
-import React, { useState } from "react";
-import avatar from '../avatar.jpg';
-import Camera from "../components/svg/camera";
+import React, { useState, useEffect } from "react";
+import Avatar from '../user.jpg';
+import Camera from "../components/svg/Camera";
+import Delete from "../components/svg/Delete";
+import { storage, db, auth } from "../firebase";
+import { ref, getDownloadURL, uploadBytes, deleteObject } from 'firebase/storage';
+import { getDoc, doc, updateDoc } from "firebase/firestore";
 
 const Profile = () => {
     const [img, setImg] = useState('')
-    return (
+    const [user, setUser] = useState('');
+
+    useEffect(() => {
+        getDoc(doc(db, 'users', auth.currentUser.uid))
+            .then((docSnap) => {
+                if (docSnap.exists) {
+                    setUser(docSnap.data());
+                }
+            });
+        if (img) {
+            const uploadImg = async () => {
+                const imgRef = ref(
+                    storage,
+                    `avatar/${new Date().getTime()} - ${img.name}`
+                );
+
+
+                try {
+
+                    if (user.avatarPath) {
+                        await deleteObject(ref(storage, user.avatarPath))
+                    }
+
+                    const snap = await uploadBytes(imgRef, img);
+                    const url = await getDownloadURL(ref(storage, snap.ref.fullPath))
+                    await updateDoc(doc(db, 'users', auth.currentUser.uid), {
+                        avatar: url,
+                        avatarPath: snap.ref.fullPath
+                    });
+
+                    setImg('');
+                } catch (err) {
+                    console.log(err)
+                }
+
+            };
+            uploadImg();
+
+        }
+    }, [img])
+    return user ? (
         <section>
             <div className="profile_container">
                 <div className="img_container">
-                    <img src={avatar} alt="" />
+                    <img src={user.avatar || Avatar} alt="" />
                     <div className="overlay">
                         <div>
                             <label htmlFor="photo">
                                 <Camera />
                             </label>
-                            <input type="file" accept="image/*" style={{ display: 'none' }} id='photo' />
+                            {user.avatar ? <Delete /> : null}
+                            <input type="file" accept="image/*" style={{ display: 'none' }} id='photo' onChange={e => setImg(e.target.files[0])} />
                         </div>
                     </div>
                 </div>
                 <div className="text_container">
-                    <h3>Ruslan Orlov</h3>
-                    <p>User email</p>
+                    <h3>{user.name}</h3>
+                    <p>{user.email}</p>
                     <hr />
-                    <small>Joined on: ...</small>
+                    <small>Joined on: {user.createdAt.toDate().toDateString()}</small>
                 </div>
             </div>
         </section>
-    )
+    ) : null
 };
 
 export default Profile;
