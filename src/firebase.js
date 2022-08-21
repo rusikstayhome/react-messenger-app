de104abb0c9329f874e03b277bf1a7d1a295a93b
rusikstayhome <alenrus32@gmail.com>
2022-08-21 20:27:37 +0300

    transfered from env

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: 'AIzaSyB0hyupm_y6_WYYGm1prq1sLBbq89phX5s',
+    authDomain: 'react-messenger-62ce7.firebaseapp.com',
+    databaseURL: 'http://react-messenger-62ce7.firebaseio.com',
+    projectId: 'react-messenger-62ce7',
+    storageBucket: 'react-messenger-62ce7.appspot.com',
+    messagingSenderId: '416061635243',
+    appId: '1:416061635243:web:815ded1e1edb83bc6c95ee',
 };
 
 // Initialize Firebase
